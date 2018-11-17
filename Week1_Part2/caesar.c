b94ce13821f2c789e006bbe09b8add64d3408b49
zseen <43151120+zseen@users.noreply.github.com>
2018-11-17 16:13:36 +0000

    Refactor exercise "caesar"
    - Use char* instead of string
    - Use a constant for the length of the alphabet

--- Week1_Part2/caesar.c
+++ Week1_Part2/caesar.c
@@ -3,34 +3,44 @@
 #include <string.h>
 #include <ctype.h>
 
+const unsigned lenAlphabet = 26;
 
-int main(int argc, string argv[])
+int main(int argc, char* argv[])
 {
     if (argc != 2)
     {
-    return 1;
+        return 1;
     }
 
     int key = atoi(argv[1]);
-    string plaintext = get_string("Give me a string to cipher: ");
-    char ciphertext[strlen(plaintext)];
+    char* plaintext = get_string("Give me a string to cipher: ");
+    char* ciphertext = (char*)malloc((strlen(plaintext) + 1) * sizeof(char));
+
+    if (ciphertext == NULL)
+    {
+        return 1;
+    }
 
     for (int i = 0; i < strlen(plaintext); i++)
     {
         if (isupper(plaintext[i]))
         {
-            ciphertext[i] = ((plaintext[i] + key) - 65) % 26 + 65;
+            ciphertext[i] = ((plaintext[i] + key) - 'A') % lenAlphabet + 'A';
         }
-
         else if (islower(plaintext[i]))
         {
-            ciphertext[i] = ((plaintext[i] + key) - 97) % 26 + 97;
+            ciphertext[i] = ((plaintext[i] + key) - 'a') % lenAlphabet + 'a';
         }
         else
         {
             ciphertext[i] = plaintext[i];
         }
     }
+
+    ciphertext[strlen(plaintext)] = '\0';
+
     printf("%s: " "%s\n", "plaintext", plaintext);
     printf("%s: " "%s\n", "ciphertext", ciphertext);
+
+    free(ciphertext);
 }
