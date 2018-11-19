cd0f0966bc279158af35b9ea87b4a8dea0ca1907
zseen <43151120+zseen@users.noreply.github.com>
2018-11-19 13:32:27 +0000

    Refactor exercise "vigenere"

--- Week1_Part2/Vigenere/vigenere.c
+++ Week1_Part2/Vigenere/vigenere.c
@@ -3,19 +3,22 @@
 #include <string.h>
 #include <ctype.h>
 
-
-int main(void)
+int main(int argc, char* argv[])
 {
-    //for (int i = 0; i < strlen(argv[1]); i++)
-    //{
-        //if (!isalpha(argv[1][i]) || argc != 2)
-        //{
-            //return 1;
-        //}
-
-    //}
-    char* key = "BaZ";
-    //printf("%s", key);
+    if (argc != 2)
+    {
+        printf("Please give a key to encrypt with!\n");
+        return 1;
+    }
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!isalpha(argv[1][i]))
+        {
+            printf("Your key should only consist of letters!");
+            return 1;
+        }
+    }
+    char* key = argv[1];
     char* plaintext = get_string("Give me a string to cipher: ");
     char* ciphertext = (char*)malloc((strlen(plaintext) + 1) * sizeof(char));
     
@@ -23,36 +26,18 @@ int main(void)
 
     for (int i = 0, k = 0; i < strlen(plaintext); i++)
     {
-        if (isupper(plaintext[i]))
+        if (isalpha(plaintext[i]))
         {
-            int keychar;
-            if (isupper(key[i % keyLength]))
-            {
-                keychar = (key[k++ % keyLength] - 'A') % 26;
-            }
-            else
-            {
-                keychar = (key[k++ % keyLength] - 'a') % 26;
-            }
-            //int keychar = toupper(key[k++ % keyLength] - 'A') % 26;
-            //printf("%i\n", keychar);
-            ciphertext[i] = ((plaintext[i] + keychar) - 'A') % 26 + 'A';
-          
-        }
+            int keychar = (toupper(key[k++ % keyLength]) - 'A') % 26;
 
-        else if (islower(plaintext[i]))
-        {
-            int keychar;
-            if (isupper(key[i % keyLength]))
+            if (isupper(plaintext[i]))
             {
-                keychar = (key[k++ % keyLength] - 'A') % 26;
+                ciphertext[i] = ((plaintext[i] + keychar) - 'A') % 26 + 'A';
             }
             else
-            {
-                keychar = (key[k++ % keyLength] - 'a') % 26;
+            {  
+                ciphertext[i] = ((plaintext[i] + keychar) - 'a') % 26 + 'a';
             }
-           
-            ciphertext[i] = ((plaintext[i] + keychar) - 'a') % 26 + 'a';
         }
         else
         {
@@ -64,5 +49,4 @@ int main(void)
     printf("%s: " "%s\n", "ciphertext", ciphertext);
 
     free(ciphertext);
-
 }
