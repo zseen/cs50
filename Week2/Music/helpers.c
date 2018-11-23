ff06089226b76095c71be40e6dcc6124f87ee1c1
zseen <43151120+zseen@users.noreply.github.com>
2018-11-23 18:53:40 +0000

    Calculate the frequency from A0 to A8 with '#' and 'b'

--- Week2/Music/helpers.c
+++ Week2/Music/helpers.c
@@ -1,9 +1,11 @@
 // Helper functions for music
 
 #include <cs50.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "helpers.h"
-#include<math.h>
+#include <math.h>
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
@@ -17,31 +19,50 @@ int duration(string fraction)
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    //printf("%c", note[strlen(note) - 1]);
+    //printf("%s", note);
     int A4 = 440;
     int numSeminotes = 12;
 
-
-
-    //int A1 = pow(2, - 3 * 12 / 12) * 440; //pow(2, - (4 - 1) * 12 / 12) * 440;  pow(2, (Y - A4) * numSeminotes) * 440;
-    //int A2 = pow(2, -2 * 12 / 12) * 440;
     int noteLength = strlen(note);
     char letter = note[0];
     char number;
+    //printf("%c", letter);
     number = note[strlen(note) - 1];
     //printf("%c", number);
-    int frequency;
 
-    for (int i = 0; i < number - 48; i++)
+    
+
+
+    float frequency;
+
+    for (int i = 0; i < number - 47; i++)
     {
+        //printf("%d", i);
+        if (noteLength == 3)
         {
-            frequency = pow(2, ((i + 1 - 4) * numSeminotes / 12)) * 440;
+          
+            char modifier = note[strlen(note) - 2];
+            double semiTone = pow(2, (1.0 / 12.0));
+        
+          
+            if (modifier == '#')
+            {
+                
+                frequency = round(pow(2, (i - 4)) * 440 * semiTone);
+                
+            }
+            else
+            {
+                frequency = round(pow(2, (i - 4)) * 440 / semiTone);           
+            }
         }
-
-    }
+        else
+        {
+            frequency = round(pow(2, (i - 4)) * 440);           
+        }
+    }  
     return frequency;
 }
-
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
