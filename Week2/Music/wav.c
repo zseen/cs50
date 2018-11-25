6b87b7bdde20405354d368dc8079413d6c95dff6
zseen <43151120+zseen@users.noreply.github.com>
2018-11-25 22:07:31 +0000

    Use binary mode to open a wav file

--- Week2/Music/wav.c
+++ Week2/Music/wav.c
@@ -64,7 +64,7 @@ static FILE *wav_open(const char *filename)
     header.block_align = bits_per_sample / 8;
     header.bits_per_sample = bits_per_sample;
     header.data_length = 0;
-    FILE *file = fopen(filename, "w+");
+    FILE *file = fopen(filename, "w+b");
     if (!file)
     {
         return NULL;
