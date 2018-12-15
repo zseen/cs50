7df0553450ac39d766dc0e344ba8d3fb95927462
zseen <43151120+zseen@users.noreply.github.com>
2018-12-15 18:10:03 +0000

    Week4 - Implement "check" and "size" - run on small dict and cat.txt

--- Week4/speller/dictionary.c
+++ Week4/speller/dictionary.c
@@ -4,17 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <ctype.h>
 #include "dictionary.h"
 
 const int MAX_HASH = 27;
-node* hashTable = NULL;
+node** hashTable = NULL;
+int wordsInDictCounter;
 
 void makeHashTable()
 {
-    hashTable = (node*)malloc(sizeof(node) * MAX_HASH);
+    hashTable = (node**)malloc(sizeof(node) * MAX_HASH);
 }
 
+int getHashedValue(const char* currentWord) /// maybe node.word?
+{
+    int sumOfChars = 0;
+    for (int charPosition = 0; charPosition < strlen(currentWord); charPosition++)
+    {
+        sumOfChars += currentWord[charPosition];
+    }
+
+    int hashedValue = sumOfChars % MAX_HASH;
+    return hashedValue;
+}
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
@@ -25,11 +38,15 @@ bool check(const char *word)
 
     while (cursor != NULL)
     {
-        srtcasecmp();
-        cursor = cursor->next;
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            cursor = cursor->next;
+        }    
     }
-        
-
 
     return false;
 }
@@ -46,42 +63,25 @@ bool isNodeCreated(node* currentNode)
 
 node* putWordInNode(char* currentWord, node* currentNode)
 {
-    currentNode->word = malloc(strlen(currentWord) + 1);
+    currentNode->word = malloc((strlen(currentWord)) + 1);
     strcpy(currentNode->word, currentWord);
     return currentNode;
 }
 
-void linkTwoNodes(node* firstNode, node* secondNode)
-{
-    firstNode->next = secondNode;
-}
 
-int getHashedValue(char* currentWord) /// maybe node.word?
-{
-    int sumOfChars = 0;
-    for (int charPosition = 0; charPosition < strlen(currentWord); charPosition++)
-    {
-        sumOfChars += currentWord[charPosition];
-    }
-
-    int hashedValue = sumOfChars % MAX_HASH;
-    return hashedValue;
-}
-
-
-void insertNodeIntoHashTable(node* currentNode, node* hashTable)
+void insertNodeIntoHashTable(node* currentNode)
 {
     int index = getHashedValue(currentNode->word);
-    node* head = &hashTable[index];
 
-    if (head == NULL)
+
+    if (hashTable[index] == NULL)
     {
-        head = currentNode;
+        hashTable[index] = currentNode;
     }
     else
     {
-        currentNode->next = head;
-        head = currentNode;
+        currentNode->next = hashTable[index];
+        hashTable[index] = currentNode;
     }
 }
 
@@ -96,21 +96,20 @@ bool load(const char *dictionary)
         return false;
     }
 
-
     char currentWord[LENGTH + 1];
     //node* hashTable = (node*)malloc(sizeof(node) * MAX_HASH);
 
-    node* hashTable = makeHashTable();
+    makeHashTable();
 
     while (fscanf(file, "%s", currentWord) != EOF)
     {
-
         node* currentNode = malloc(sizeof(node));
         if (isNodeCreated(currentNode))
         {
             node* readyNode = putWordInNode(currentWord, currentNode);
-            insertNodeIntoHashTable(readyNode, hashTable);
-            printf("%s", readyNode->word);
+            insertNodeIntoHashTable(readyNode);
+            //printf("%s", readyNode->word);
+            wordsInDictCounter++;
         }
         else
         {
@@ -123,30 +122,12 @@ bool load(const char *dictionary)
 
     return true;
 
-    /*char word[LENGTH + 1] = {};
-    int wordLength = strlen(word);
-
-    create a char array node, with pointer to the next node
-    make linked list strcpy(node1->word, "Hello"), then node1->next = node2
-    insert node into hash table : -look up where to put it(alphabeticall ? )
-    - add it to linked list nodes
-    hash table : node * hashtable[]
-
-    for every word we scan - malloc a new node, node* new_node = malloc(sizeof(node)),
-        if new_node false, unload, return false, else copy word into node with strcpy(new_word->word, word)
-
-
-    hash function (): */
-
-
-    // TODO
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return wordsInDictCounter;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
