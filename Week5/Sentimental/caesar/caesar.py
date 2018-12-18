df04e08b627e3790b266e970e78a46f8bf212f2f
zseen <43151120+zseen@users.noreply.github.com>
2018-12-18 16:49:22 +0000

    Week5 - Refactor caesar.py to handle non-letter characters and commandline input

--- Week5/Sentimental/caesar/caesar.py
+++ Week5/Sentimental/caesar/caesar.py
@@ -1,3 +1,6 @@
+import sys
+
+
 ALPHABET_LENGTH = 26
 
 def encriptWord(word, key):
@@ -6,12 +9,12 @@ def encriptWord(word, key):
     ascii_a = 97
 
     for char in word:
-        if char == char.upper():
-            appendLetterToEncriptedWord(ascii_A, char, encriptedWord, key)
-        elif char == char.lower():
-            appendLetterToEncriptedWord(ascii_a, char, encriptedWord, key)
-        else:
+        if not char.isalpha():
             encriptedWord.append(char)
+        elif char == char.upper():
+            appendLetterToEncriptedWord(ascii_A, char, encriptedWord, key)
+        else:
+            appendLetterToEncriptedWord(ascii_a, char, encriptedWord, key)
 
     return ''.join(encriptedWord)
 
@@ -22,13 +25,17 @@ def appendLetterToEncriptedWord(ascii_aOrA, char, encriptedWord, key):
 
 
 def main():
-    key = (input("Please give me a numerical key to encript with!: "))
+    if len(sys.argv) != 2:
+        exit(1)
+
+    key = sys.argv[1]
     while not key.isdigit():
         key = (input("Please give me a numerical key to encript with!: "))
 
-    word = input("Give me a word to cipher!: ")
-    encriptedWord = encriptWord(word, int(key))
-    print(encriptedWord)
+    plaintext = input("Give me a word to cipher!: ")
+    ciphertext = encriptWord(plaintext, int(key))
+    print("plaintext: ", plaintext)
+    print("ciphertext: ", ciphertext)
 
 
 if __name__ == '__main__':
