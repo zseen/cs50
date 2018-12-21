640d36c503b091eb7349436ee2cb142f699ebd11
zseen <43151120+zseen@users.noreply.github.com>
2018-12-21 16:33:46 +0000

    Week5 - Start implementing functions in helpers.py

--- Week5/similarities/helpers.py
+++ Week5/similarities/helpers.py
@@ -1,19 +1,53 @@
+from nltk.tokenize import sent_tokenize
+
+
 def lines(a, b):
     """Return lines in both a and b"""
+    linesInA = a.splitlines("\n")
+    linesInB = b.splitlines("\n")
 
-    # TODO
-    return []
+    commonLinesList = []
+    for line in linesInA:
+        if line in linesInB and line not in commonLinesList:
+            commonLinesList.append(line)
+
+    return commonLinesList
 
 
 def sentences(a, b):
     """Return sentences in both a and b"""
 
-    # TODO
-    return []
+    sentencesInA = sent_tokenize(a, language='english')
+    sentencesInB = sent_tokenize(b, language='english')
+
+    commonSentencesList = []
+    for sentence in sentencesInA:
+        if sentence in sentencesInB and sentence not in commonSentencesList:
+            commonSentencesList.append(sentence)
+
+    return commonSentencesList
+
+
+def getSubstringsInString(string, size):
+    substringsList = []
+    for char in range(0, len(string) - size):
+        substringsList.append(string[char: char + size])
+
+    return substringsList
 
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
 
-    # TODO
-    return []
+    substringsOfFirstString = getSubstringsInString(a, n)
+    substringsOfSecondString = getSubstringsInString(b, n)
+
+    commonSubstringsList = []
+    for substring in substringsOfFirstString:
+        if substring in substringsOfSecondString and substring not in commonSubstringsList:
+            commonSubstringsList.append(substring)
+
+    return commonSubstringsList
+
+
+
