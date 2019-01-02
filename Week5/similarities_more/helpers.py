270866820c77358af129498b3f7d2ef9f88b223d
zseen <43151120+zseen@users.noreply.github.com>
2019-01-02 22:06:26 +0000

    Week5 - Add a working version of helpers.py, accepted by check50

--- Week5/similarities_more/helpers.py
+++ Week5/similarities_more/helpers.py
@@ -14,6 +14,33 @@ class Operation(Enum):
 
 def distances(a, b):
     """Calculate edit distance from a to b"""
+    rows = len(a) + 1
+    columns = len(b) + 1
+    cost = [[(0, None)] * columns for i in range(rows)]
 
-    # TODO
-    return [[]]
+    for row in range(1, rows):
+        cost[row][0] = (row, Operation.DELETED)
+    for column in range(1, columns):
+        cost[0][column] = (column, Operation.INSERTED)
+
+    for i in range(1, rows):
+        for j in range(1, columns):
+            deletionCost = cost[i-1][j]
+            insertionCost = cost[i][j-1]
+            substitutionCost = cost[i-1][j-1]
+
+            substCost = substitutionCost[0]
+            if a[i - 1] == b[j - 1]:
+                minCost = min(deletionCost[0] + 1, insertionCost[0] + 1, substCost)
+            else:
+                substCost += 1
+                minCost = min(deletionCost[0] + 1, insertionCost[0] + 1, substCost)
+
+            if minCost == deletionCost[0] + 1:
+                cost[i][j] = (deletionCost[0] + 1, Operation.DELETED)
+            elif minCost == insertionCost[0] + 1:
+                cost[i][j] = (insertionCost[0] + 1, Operation.INSERTED)
+            else:
+                cost[i][j] = (substCost, Operation.SUBSTITUTED)
+
+    return cost
